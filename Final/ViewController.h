64467bba8c528a8825f332d365602425b7f1c6b0
jhamer02 <jhamer02@hotmail.com>
2016-08-30 22:47:09 -0500

    Initial Commit

---
+++ Final/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Final
+//
+//  Created by Jorge Jhamil Pineda Echeverria on 30/08/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Final/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Final
+//
+//  Created by Jorge Jhamil Pineda Echeverria on 30/08/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
